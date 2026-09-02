358931dbde195a37dc25854a81fdf3f96f36ef3c
Emil Velken Soldal <emil.v.soldal@gmail.com>
2026-09-02 10:03:21 +0200

    Add staffed information to currentRoute in detailedTrainInfo endpoint

--- app.js
+++ app.js
@@ -1201,6 +1201,17 @@ app.get('/fido/detailedTrainInfo/:trainNumber', checkApiKey, async (req, res) =>
             endDate: { $gte: now }
         }).exec();
 
+        // Add the staffed info inside the currentRoute of the trainInfo, if not found dont add anything
+
+        trainInfo.currentRoute = await Promise.all(trainInfo.currentRoute.map(async stop => {
+            const staffed = await fidoStaffed.findOne({ stationCode: stop.code }).exec();
+            if (staffed) {
+                stop.staffed = staffed.staffed;
+                stop.staffingType = staffed.staffingType;
+            }
+            return stop;
+        }));
+
         res.status(200).json({
             trainInfo,
             claim,
